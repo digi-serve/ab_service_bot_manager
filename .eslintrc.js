ef6dd5b2e7629ae604b7359ebdfc682c086fe108
nate <7259@pm.me>
2022-01-25 15:05:34 +0700

    [add] eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+   root: true,
+   env: {
+      node: true,
+      browser: true,
+      es6: true,
+   },
+
+   parserOptions: {
+      ecmaVersion: 8,
+      sourceType: "module",
+   },
+
+   // "parser": "babel-eslint",
+   extends: ["eslint:recommended", "prettier"], // extending recommended config and config derived from eslint-config-prettier
+
+   globals: {
+      webix: "readonly",
+      $$: "readonly",
+   },
+
+   plugins: ["prettier"],
+   rules: {
+      "prettier/prettier": [
+         // customizing prettier rules (unfortunately not many of them are customizable)
+         "error",
+         {
+            arrowParens: "always",
+            endOfLine: "lf",
+            printWidth: 80,
+            tabWidth: 3,
+         },
+      ],
+      "no-console": 0, // "off",
+      // eqeqeq: ["error", "always"] // adding some custom ESLint rules
+   },
+};
